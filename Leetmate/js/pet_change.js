7c0b7d0dd82c9a8bb2614dd7e4f45ef179c24dcb
Amilio21 <132502878+Amilio21@users.noreply.github.com>
2026-03-15 04:18:24 -0700

    updates name of pet when changing

--- Leetmate/js/pet_change.js
+++ Leetmate/js/pet_change.js
@@ -7,14 +7,18 @@ function changePet() {
     //var element = document.getElementById("active-pet");
     //element.classList.toggle("home-hero-pet.bat");
     let pet = document.getElementById("active-pet");
+    let petName = document.getElementById("pet-name-display");
     if (pet.classList != "home-hero-pet") {
         pet.classList.remove("home-hero-pet-bat");
         pet.classList.add("home-hero-pet");
+        petName.innerText = "Fluffy Cat";
+
     }
     else
     {
         pet.classList.remove("home-hero-pet");
         pet.classList.add("home-hero-pet-bat");
+        petName.innerText = "Funny Bat";
     }
 }
 
