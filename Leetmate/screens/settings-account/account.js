4e9abb3f66fc710d2c6383bb11ea98bb8e9e1fb2
Helen Ton <helen.ton01@student.csulb.edu>
2026-03-24 00:13:14 -0700

    reorganize Account Info UI: add dropdown sections, center profile, & use screen banner

--- Leetmate/screens/settings-account/account.js
+++ Leetmate/screens/settings-account/account.js
@@ -1,27 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-const profileClick = document.getElementById("profile-click");
-const backBtn = document.getElementById("back-btn");
+    const profileClick = document.getElementById("profile-click");
+    const backBtn = document.getElementById("back-btn");
 
-if (profileClick) {
-    profileClick.addEventListener("click", () => {
-    window.location.href = "../settings-profile-color/index.html";
-    });
-}
-
-if (backBtn) {
-    backBtn.addEventListener("click", () => {
-    if (window.history.length > 1) {
-        window.history.back();
-    } else {
-        window.location.href = "../settings-main/index.html";
+    if (profileClick) {
+        profileClick.addEventListener("click", () => {
+        window.location.href = "../settings-profile-color/index.html";
+        });
     }
-    });
-}
+
+    if (backBtn) {
+        backBtn.addEventListener("click", () => {
+            if (window.history.length > 1) {
+                window.history.back();
+            } else {
+                window.location.href = "../settings-main/index.html";
+            }
+        });
+    }
 });
 
-const updateButton = document.getElementById('submit-updates');
-updateButton.addEventListener('click', function () {
-    message = document.getElementById('update-email').value;
-    //alert(message);
-    alert(storageGet("uid"));
+document.addEventListener("DOMContentLoaded", () => {
+    const updateButton = document.getElementById('submit-updates');
+  
+    if (updateButton) {
+      updateButton.addEventListener('click', () => {
+        const message = document.getElementById('update-email').value;
+        alert(storageGet("uid"));
+      });
+    }
+});
+
+document.querySelectorAll(".accordion-header").forEach(header => {
+    header.addEventListener("click", () => {
+        const item = header.parentElement;
+
+        item.classList.toggle("open");
+    });
 });
