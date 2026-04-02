fe4da365c9cba37d16d192f84fafda86cc39e634
Janet Ayala <janet.ayala916@gmail.com>
2026-04-02 00:16:00 -0700

    Updated community menu theme and routing

--- Leetmate/screens/community-main/community.js
+++ Leetmate/screens/community-main/community.js
@@ -4,14 +4,18 @@
 (function () {
   'use strict';
 
-  var dest = '../community-green/index.html';
   var ids = ['multiplayer-btn', 'leaderboard-btn', 'friends-btn'];
+  var routes = {
+    'multiplayer-btn': '../community-multiplayer/index.html',
+    'leaderboard-btn': '../community-leaderboard/index.html',
+    'friends-btn': '../community-friends/index.html'
+  };
 
   ids.forEach(function (id) {
     var el = document.getElementById(id);
     if (!el) return;
     el.addEventListener('click', function () {
-      window.location.href = dest;
+      window.location.href = routes[id];
     });
   });
 })();
